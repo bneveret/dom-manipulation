73fedc1b85de135661de3ffcb0c289289d208615
bneveret <eve20003@byui.edu>
2022-02-03 20:45:14 -0700

    final draft

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('#add');
 const list = document.querySelector('ul');
-const deleteButton = document.querySelector('#deleteButton');
 
 input.focus();
 
@@ -12,17 +11,16 @@ button.addEventListener('click', function() {
         let li = document.createElement('li');
         let span = document.createElement('span');
         let button2 = document.createElement('button');
-        button2.setAttribute('id','deleteButton');
         li.appendChild(span);
         li.appendChild(button2);
         span.textContent = userInput;
         button2.textContent = 'X';
         list.appendChild(li);
+
+        button2.onclick = function(e) {
+            list.removeChild(li);
+          }
+
         input.focus();
     }
-})
-
-deleteButton.addEventListener('click', function() {
-    deleteButton.parentElement.remove();
-
-})
+});
